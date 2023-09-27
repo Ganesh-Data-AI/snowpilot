2a762daac925592b3276a8ad10b34027975c6b7c
GANESH70755 <104563834+GANESH70755@users.noreply.github.com>
2023-09-27 13:05:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,38 @@
+
+# conn_params = {
+# "OPENAI_API_KEY" : "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",
+# "account" : "LG95411",
+# "region" : "central-india.azure",
+# "user" : "GANESHK",
+# "password" : "snowflakeK@9392",
+# "database" : "SNOWPILOT_DB",
+# "db_schema" : "SNOWPILOT_SCHEMA"
+# }
+
+
+# conn_params = {
+# "OPENAI_API_KEY" : "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",
+# "account" : "PX69115",
+# "region" : "east-us-2.azure",
+# "user" : "VRUSHANG",
+# "password" : "Vrushang@123",
+# "database" : "SNOWIOTB",
+# "db_schema" : "SNOWIOTB_SCHEMA"
+# }
+
+
+conn_params = {
+"OPENAI_API_KEY" : "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",
+"account" : "anblicksorg_aws",
+"region" : "us-east-1",
+"user" : "SNOWIOT",
+"password" : "SnowIOT@202308",
+"database" : "SNOWIOT_DB",
+"db_schema" : "MAIN"
+}
+
+
+# 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
+# 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
+# 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
+# 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
