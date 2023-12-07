4b77def773906e8976e97f98fe083fdcab4478c8
GANESH70755 <104563834+GANESH70755@users.noreply.github.com>
2023-12-07 15:13:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,19 @@
+# conn_params = {
+# "account" : "anblicksorg_aws",
+# "region" : "us-east-1",
+# "user" : "SNOWIOT",
+# "password" : "SnowIOT@202308",
+# "database" : "SNOWIOT_DB",
+# "db_schema" : "MAIN"
+# }
+
+
 conn_params = {
-"account" : "anblicksorg_aws",
-"region" : "us-east-1",
-"user" : "SNOWIOT",
-"password" : "SnowIOT@202308",
-"database" : "SNOWIOT_DB",
-"db_schema" : "MAIN"
+"OPENAI_API_KEY" : "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",
+"account" : "KB93590",
+"region" : "ap-southeast-1",
+"user" : "Nandini19",
+"password" : "Nandy@#19",
+"database" : "SNOWPILOT",
+"db_schema" : "PILOT_SCHEMA"
 }
