6d73224376f8b2ffcf21c4c79d060a6daaa6e8e9
CodeWithGanesh <104563834+Ganesh-Data-AI@users.noreply.github.com>
2025-07-31 14:50:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,10 @@
-# conn_params = {
-# "account" : "anblicksorg_aws",
-# "region" : "us-east-1",
-# "user" : "SNOWIOT",
-# "password" : "SnowIOT@202308",
-# "database" : "SNOWIOT_DB",
-# "db_schema" : "MAIN"
-# }
-
+import os
 
 conn_params = {
-"account" : "KB93590",
-"region" : "ap-southeast-1",
-"user" : "Nandini19",
-"password" : "Nandy@#19",
-"database" : "SNOWPILOT",
-"db_schema" : "PILOT_SCHEMA"
+    "account": os.environ.get("SNOWFLAKE_ACCOUNT"),
+    "region": os.environ.get("SNOWFLAKE_REGION"),
+    "user": os.environ.get("SNOWFLAKE_USER"),
+    "password": os.environ.get("SNOWFLAKE_PASSWORD"),
+    "database": os.environ.get("SNOWFLAKE_DATABASE"),
+    "db_schema": os.environ.get("SNOWFLAKE_DB_SCHEMA")
 }
